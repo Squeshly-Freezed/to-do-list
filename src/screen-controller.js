b6c1a29aee75b9ebbab1ec022b72ba11334ee7ec
Squeshly-Freezed <251692768+Squeshly-Freezed@users.noreply.github.com>
2026-05-03 07:12:03 +1200

    add flex styling, and background colors

--- src/screen-controller.js
+++ src/screen-controller.js
@@ -1,9 +1,12 @@
 import AppState, { proj } from "./app-state.js";
 import "./styles.css";
 import background from "./img/desk-background.webp";
+import logo from "./img/squeshly-freezed-v2-transparent.png";
 
 const mainContainer = document.querySelector(".main-container");
 mainContainer.style.backgroundImage = `url(${background})`;
+const logoImage = document.querySelector(".logo");
+logoImage.src = logo;
 
 export default class ScreenController {
     
